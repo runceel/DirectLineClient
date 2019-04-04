7a20b496a1f71cfb775b99f46cef08aaf6aad5b1
Kazuki Ota <kaota@microsoft.com>
2019-04-04 21:05:00 +0900

    add bot logic

--- Chatbot.Server/DirectBot.cs
+++ Chatbot.Server/DirectBot.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,77 @@ namespace Chatbot.Server
 {
     public class DirectBot : IBot
     {
-        public Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        public async Task OnTurnAsync(ITurnContext context, CancellationToken cancellationToken = default(CancellationToken))
         {
-            return Task.CompletedTask;
+            // Respond to the various activity types.
+            switch (context.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    // Respond to the incoming text message.
+                    await RespondToMessageAsync(context);
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    break;
+
+                case ActivityTypes.ContactRelationUpdate:
+                    break;
+
+                case ActivityTypes.Typing:
+                    break;
+
+                case ActivityTypes.DeleteUserData:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Responds to the incoming message by either sending a hero card, an image, 
+        /// or echoing the user's message.
+        /// </summary>
+        /// <param name="context">The context of this conversation.</param>
+        private async Task RespondToMessageAsync(ITurnContext context)
+        {
+            switch (context.Activity.Text.Trim().ToLower())
+            {
+                case "hi":
+                case "hello":
+                case "help":
+                    // Send the user an instruction message.
+                    await context.SendActivityAsync("Welcome to the Bot to showcase the DirectLine API. " +
+                    "Send \"Show me a hero card\" or \"Send me a BotFramework image\" to see how the " +
+                    "DirectLine client supports custom channel data. Any other message will be echoed.");
+                    break;
+
+                case "show me a hero card":
+                    // Create the hero card.
+                    HeroCard heroCard = new HeroCard()
+                    {
+                        Title = "Sample Hero Card",
+                        Text = "Displayed in the DirectLine client"
+                    };
+
+                    // Attach the hero card to a new activity.
+                    await context.SendActivityAsync(MessageFactory.Attachment(heroCard.ToAttachment()));
+                    break;
+
+                case "send me a botframework image":
+                    // Create the image attachment.
+                    var imageAttachment = new Attachment()
+                    {
+                        ContentType = "image/png",
+                        ContentUrl = "https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png",
+                    };
+
+                    // Attach the image attachment to a new activity.
+                    await context.SendActivityAsync(MessageFactory.Attachment(imageAttachment));
+                    break;
+
+                default:
+                    // No command was encountered. Echo the user's message.
+                    await context.SendActivityAsync($"You said \"{context.Activity.Text}\"");
+                    break;
+            }
         }
     }
 }
