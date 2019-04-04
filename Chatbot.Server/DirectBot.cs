5de6aee68c627abcae2dadc7ef0d293a0ffa07a5
Kazuki Ota <kaota@microsoft.com>
2019-04-04 21:02:21 +0900

    add bot class and startup logic

---
+++ Chatbot.Server/DirectBot.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chatbot.Server
+{
+    public class DirectBot : IBot
+    {
+        public Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- Chatbot.Server/Startup.cs
+++ Chatbot.Server/Startup.cs
@@ -5,19 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Configuration;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Chatbot.Server
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IHostingEnvironment _env;
+
+        public Startup(IHostingEnvironment env)
         {
+            _env = env;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var botConfig = BotConfiguration.Load("DirectBot.bot");
+            services.AddSingleton(botConfig);
+
+            services.AddBot<DirectBot>(options =>
+            {
+                var endpoint = botConfig.Services
+                    .Where(x => x.Type == "endpoint")
+                    .Where(x => x.Name == (_env.IsProduction() ? "production" : "development"))
+                    .OfType<EndpointService>()
+                    .FirstOrDefault() ?? throw new InvalidOperationException("The endpoint not found.");
+                options.CredentialProvider = new SimpleCredentialProvider(endpoint.AppId, endpoint.AppPassword);
+            });
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,10 +44,9 @@ namespace Chatbot.Server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseBotFramework();
         }
     }
 }
